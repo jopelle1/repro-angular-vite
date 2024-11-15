9c8a8438d48b6a700c17b4f4c2997a5f89c91557
Joseph Pellegrini <joseph@m19.com>
2024-11-15 13:59:57 +0100

    add test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +8,33 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'repro';
+  totalTime: string = "";
+
+  private computeTotalData(dailyStats: any[]): any {
+    return dailyStats.map((m) => ({...m}));
+  }
+
+  runTest() {
+    const data: any[] = [];
+    for (let k = 0; k < 200_000; k++) {
+      data.push({abc1: 0});
+    }
+
+    const start = new Date();
+    this.computeTotalData(data);
+    const end = new Date();
+    return end.getTime() - start.getTime();
+  }
+
+  ngOnInit() {
+    const nbTests = 10;
+    let totalTime = 0;
+    for (let i = 0; i < nbTests; i++) {
+      totalTime += this.runTest();
+    }
+
+    this.totalTime = `Average : ${totalTime / nbTests} ms`;
+  }
 }
